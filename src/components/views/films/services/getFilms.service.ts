dc23941bcebea9e3c89b1ffb6343644f6d0a33a1
Edwin Ortiz <edarcode@gmail.com>
2024-11-07 02:18:35 -0500

    agg otra pagina+

--- src/components/views/films/services/getFilms.service.ts
+++ src/components/views/films/services/getFilms.service.ts
@@ -1,12 +1,12 @@
 import type { Films } from "../types";
 
-export const getFilmsService = async (params: Params = {}) => {
+export const getFilmsService = async ({ page = 1, sizeImg }: Params = {}) => {
   const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=es-ES&page=1`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=es-ES&page=${page}`;
 
   const res = await fetch(url);
   const films = (await res.json()) as Films;
-  const size = `w${params?.sizeImg || 300}`;
+  const size = `w${sizeImg || 300}`;
   const baseImageUrl = `https://image.tmdb.org/t/p/${size}`;
 
   return {
@@ -17,4 +17,5 @@ export const getFilmsService = async (params: Params = {}) => {
 
 type Params = {
   sizeImg?: number;
+  page?: number;
 };
