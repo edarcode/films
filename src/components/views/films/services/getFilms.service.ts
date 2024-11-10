a8bb4ec141461fa454b302f90307b0af2703a30a
Edwin Ortiz <edarcode@gmail.com>
2024-11-09 23:18:19 -0500

    pintando primeras peliculas

--- src/components/views/films/services/getFilms.service.ts
+++ src/components/views/films/services/getFilms.service.ts
@@ -1,21 +1,26 @@
 import type { Films } from "../types";
 
-export const getFilmsService = async ({ page = 1, sizeImg }: Params = {}) => {
+export const getFilmsService = async ({ page = 1 }: Params = {}) => {
   const TMDB_API_KEY = import.meta.env.TMDB_API_KEY;
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=es-ES&page=${page}`;
 
   const res = await fetch(url);
   const films = (await res.json()) as Films;
-  const size = `w${sizeImg || 300}`;
+  const size = "w300";
   const baseImageUrl = `https://image.tmdb.org/t/p/${size}`;
 
   return {
-    films,
-    baseImageUrl,
+    ...films,
+    results: films.results.map((item) => {
+      return {
+        ...item,
+        poster: `${baseImageUrl}${item.poster_path}`,
+        backdrop: `${baseImageUrl}${item.backdrop_path}`,
+      };
+    }),
   };
 };
 
 type Params = {
-  sizeImg?: number;
   page?: number;
 };
